498828d352a7357aa008a512edd23c8e72d56204
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-05-01 18:25:57 +0530

    Seeder Script to import and destroy Data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 const app = express();
+
+dotenv.config();
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is runinng ...");
+});
 
 app.get("/api/products", (req, res) => {
   res.json(products);
@@ -11,4 +22,10 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
--- frontend/src/Screens/Productscreen.js
+++ frontend/src/Screens/Productscreen.js
@@ -23,7 +23,7 @@ const Productscreen = ({ match }) => {
     };
 
     fetchProduct();
-  }, []);
+  }, [match]);
 
   return (
     <>
