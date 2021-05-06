d55bfa7706fbe68d484b5e890fe34df37219e502
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-05-06 21:10:06 +0530

    Login Screen Implementation and Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./Screens/Homescreen";
 import ProductScreen from "./Screens/Productscreen";
 import CartScreen from "./Screens/CartScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/login' component={LoginScreen} />
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
