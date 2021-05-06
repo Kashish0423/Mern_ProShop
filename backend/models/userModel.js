c3fb4aaf1ab27df335030ed596e6509aafb7001d
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-05-07 00:36:53 +0530

    Logout Route and NavBar for LoggedIn User

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,8 +27,8 @@ const userSchema = mongoose.Schema(
   }
 );
 
-userSchema.methods.matchPassword = async function (enterdPassword) {
-  return await bcrypt.compare(enterdPassword, this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 userSchema.pre("save", async function (next) {
