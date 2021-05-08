e879361e0d6fde1eb45acfce3d667528c25c89b5
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-05-08 17:43:03 +0530

    Update User Profile Endpoint Added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,8 +56,8 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @des  Auth user profile
-// @ route POST /api/users/profile
+// @des  Get user profile
+// @ route GET /api/users/profile
 // @ access Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
@@ -75,4 +75,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// @des  Update user profile
+// @ route PUT /api/users/profile
+// @ access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updateUser = await user.save();
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(updateUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
