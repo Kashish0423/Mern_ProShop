81339b91711c23f9f6b8e4b350950af6e12142a8
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-05-09 13:32:46 +0530

    Order by id backend and frontend methods

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,7 +3,7 @@ import Order from "../models/orderModel.js";
 
 // @des Create new order
 // @ route POST /api/orders
-// @ Private
+// @access Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
@@ -36,4 +36,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @des  Get order by ID
+// @ route GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not Found");
+  }
+});
+
+export { addOrderItems, getOrderById };
