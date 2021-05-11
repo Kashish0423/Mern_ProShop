b6239b3504f3408f3149a94480cb93cc34a305c3
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-05-12 00:15:02 +0530

    Product Pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,14 +32,28 @@ const App = () => {
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/search/:keyword' component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userList' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-          <Route path='/admin/productList' component={ProductListScreen} />
+          <Route
+            path='/admin/productList'
+            component={ProductListScreen}
+            exact
+          />
+          <Route
+            path='/admin/productList/:pageNumber'
+            component={ProductListScreen}
+            exact
+          />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/page/:pageNumber' component={HomeScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
+          <Route
+            path='/search/:keyword/page/:pageNumber'
+            component={HomeScreen}
+          />
         </Container>
       </main>
       <Footer />
